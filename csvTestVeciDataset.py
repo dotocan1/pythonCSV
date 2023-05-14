9544c144435302f35b514228229fcb4b27bd7575
Dominik Otočan <dominik.otocan1@gmail.com>
2023-05-15 00:48:24 +0200

    sad funkcionira i to za CSV file, mozda treba maknut nadlomke

--- csvTestVeciDataset.py
+++ csvTestVeciDataset.py
@@ -3,7 +3,29 @@ import sys
 
 def izmijenaTekstaSaRijeci():
     global zamjenaVrijednost
-    zamjenaVrijednost = input("Upisite novi sadrzaj redka: ") 
+    zamjenaVrijednost = input("Upisite novi sadrzaj redka: ")
+
+def skratiNaOdredeniBrojZnamenaka(p_recenica, maksElemenata):
+    global zamjenaVrijednost
+    words = recenica.split()
+    # print(words)
+    mySentence = []
+    brojacZnamenka = 0
+    maksElemenata = 100
+    # slazi recenicu rijec po rijec dok ne dodes do 100 znamenka
+    for x in words:
+        brojacZnamenka += len(x)
+        zadnjiElementRijeci = x[len(x)-1]
+        # ako je 100. znamenka tocka, slozi recenica
+        if(brojacZnamenka >= maksElemenata and (zadnjiElementRijeci == "." or zadnjiElementRijeci == "?" or zadnjiElementRijeci == "!")):
+            mySentence.append(x)
+            editedSentence = " ".join(mySentence)
+            # print(editedSentence)
+            zamjenaVrijednost = editedSentence
+            break
+        # ako 100. znamenka nije tocka, nastavi slagat rijeci
+        else:
+            mySentence.append(x)
 
 def unosImenaStupca():
     # global imeOriginalneDatoteke
@@ -19,7 +41,8 @@ def unosImenaStupca():
     "2) Skrati recenicu na odredeni broj slova\n")
     if odabir == "1":
         izmijenaTekstaSaRijeci()
-        print("odabrano")
+    elif odabir == "2":
+        zamjenaVrijednost = skratiNaOdredeniBrojZnamenaka(recenica,100)
     else:
         sys.exit()
 
@@ -27,6 +50,7 @@ imeStupcaZamjena = "PROPERTY"
 zamjenaVrijednost = "zamjenaVrijednost"
 imeOriginalneDatoteke = "./drugiMailTablicaCSV.csv"
 imeOutputDatoteke = "testOutputFile.csv"
+recenica = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget pellentesque felis. Ut venenatis sodales erat sed ultricies. Sed a sagittis arcu. Aenean in felis egestas, pulvinar sem id, luctus libero. Donec posuere hendrerit magna, id ultrices ante euismod sit amet. Phasellus facilisis ac enim in maximus. Nullam finibus convallis molestie. Phasellus eu purus ut nisi vehicula iaculis. Nulla finibus dapibus sapien. Cras aliquet iaculis lectus, in consequat nisi vestibulum sit amet. Vestibulum sagittis diam purus, sed condimentum lorem dapibus eget. Maecenas iaculis, tortor vel posuere tincidunt, lacus mauris maximus orci, ullamcorper dignissim tellus nisl in tortor. Aenean mollis, est."
 
 unosImenaStupca()
 with open(imeOriginalneDatoteke, newline="", encoding="utf-8") as csvfile:
